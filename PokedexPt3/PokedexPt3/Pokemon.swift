e5c27b591d31b3702da426141f0875beafa3ccdd
natehopkins <nhop5040@gmail.com>
2016-10-25 10:30:09 -0600

    Setup Storyboard/Outlets, and Made model.

---
+++ PokedexPt3/PokedexPt3/Pokemon.swift
@@ -0,0 +1,61 @@
+//
+//  Pokemon.swift
+//  PokedexPt3
+//
+//  Created by Nathan Hopkins on 10/25/16.
+//  Copyright © 2016 NathanHopkins. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private let nameKey = "name"
+    private let typeKey = "name"
+    private let spriteKey = "front_default"
+    private let movesKey = "moves"
+    
+    let spriteEndpoint: String
+    let type: String
+    let name: String
+    let moveCount: Int
+    
+    init(spriteEndpoint: String, types: String, name: String, moveCount: Int) {
+        self.spriteEndpoint = spriteEndpoint
+        self.type = types
+        self.name = name
+        self.moveCount = moveCount
+    }
+    
+    init?(dictionary: [String: Any]) {
+        
+        guard let name = dictionary[nameKey] as? String else { return nil }
+        guard let typesArray = dictionary["types"] as? [[String: Any]],
+            let typesDictionary = typesArray.first,
+            let newTypeDictionary = typesDictionary["type"] as? [String: String],
+            let typeName = newTypeDictionary[typeKey] else { return nil }
+        guard let movesArray = dictionary[movesKey] as? [[String: Any]] else { return nil }
+        guard let spritesDictionary = dictionary["sprites"] as? [String: Any], let spriteEndpoint = spritesDictionary[spriteKey] as? String else { return nil }
+        
+        self.spriteEndpoint = spriteEndpoint
+        self.moveCount = movesArray.count
+        self.name = name
+        self.type = typeName
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PokedexPt3/PokedexPt3/ViewController.swift
+++ PokedexPt3/PokedexPt3/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //============================
+    //  Mark: - Outlets
+    //============================
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberOfMoves: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
